f9169d49e314ae67bb4f615d2cfec65d0d086a80
kalu_coder <awesomecoding51@gmail.com>
2022-10-17 08:07:22 +0300

    initial commit to 0th task

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,14 @@
+# a script that creates a puppet file with
+#File path is /tmp/school
+#File permission is 0744
+#File owner is www-data
+#File group is www-data
+#File contains I love Puppet
+
+file { '/tmp/school':
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
+}
