27170d55c6bf2ba6544ab3989405b7367f377198
kalu_coder <awesomecoding51@gmail.com>
2022-11-16 22:27:51 +0300

    completed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-"""This module contains a python script that interacts with the REST api.
-The script exports a given employee data into a CSV file format"""
-from requests import get
-from sys import argv
-from csv import writer, QUOTE_ALL
+'''A script that gathers data from an API and exports it to a CSV file.
+'''
+import re
+import requests
+import sys
 
 
-if __name__ == "__main__":
-    user_info = get('https://jsonplaceholder.typicode.com/users/{}'.
-                    format(argv[1])).json()
-    user_todos = get('https://jsonplaceholder.typicode.com/todos/',
-                     params={"userId": argv[1]}).json()
+API_URL = 'https://jsonplaceholder.typicode.com'
+'''The API's URL.'''
 
-    USER_ID = str(user_info.get('id'))
-    USERNAME = user_info.get('username')
 
-    for task in user_todos:
-        TASK_COMPLETED_STATUS = task.get('completed')
-        TASK_TITLE = task.get('title')
-
-        info_list = [USER_ID, USERNAME, TASK_COMPLETED_STATUS,
-                     TASK_TITLE]
-        file_name = USER_ID + '.csv'
-
-        with open(file_name, "a") as f:
-            w = writer(f, quoting=QUOTE_ALL)
-            w.writerow(info_list)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('username')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            with open('{}.csv'.format(id), 'w') as file:
+                for todo in todos:
+                    file.write(
+                        '"{}","{}","{}","{}"\n'.format(
+                            id,
+                            user_name,
+                            todo.get('completed'),
+                            todo.get('title')
+                        )
+                    )
